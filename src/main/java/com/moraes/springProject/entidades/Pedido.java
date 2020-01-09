b5d9663d876d2f24db3945932bbe0156538d5991
Matheus Moraes <matheus.moraes111@outlook.com>
2020-01-08 23:02:47 -0300

    Enum PedidoStatus

--- src/main/java/com/moraes/springProject/entidades/Pedido.java
+++ src/main/java/com/moraes/springProject/entidades/Pedido.java
@@ -1,6 +1,7 @@
 package com.moraes.springProject.entidades;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.moraes.springProject.entidades.enums.PedidoStatus;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
@@ -15,58 +16,71 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_pedido")
 public class Pedido implements Serializable {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant momento;
-
+    
+    private Integer pedidoStatus;
+    
     @ManyToOne
     @JoinColumn(name = "cliente_FK")
     private Usuario cliente;
-
+    
     public Pedido() {
     }
-
-    public Pedido(Long id, Instant momento, Usuario cliente) {
+    
+    public Pedido(Long id, Instant momento, PedidoStatus pedidoStatus, Usuario cliente) {
         this.id = id;
         this.momento = momento;
+        setPedidoStatus(pedidoStatus);
         this.cliente = cliente;
     }
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public Instant getMomento() {
         return momento;
     }
-
+    
     public void setMomento(Instant momento) {
         this.momento = momento;
     }
-
+    
+    public PedidoStatus getPedidoStatus() {
+        return PedidoStatus.valueOf(pedidoStatus);
+    }
+    
+    public void setPedidoStatus(PedidoStatus pedidoStatus) {
+        if (pedidoStatus != null) {
+            this.pedidoStatus = pedidoStatus.getCodigo();
+        }
+    }
+    
     public Usuario getCliente() {
         return cliente;
     }
-
+    
     public void setCliente(Usuario cliente) {
         this.cliente = cliente;
     }
-
+    
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 89 * hash + Objects.hashCode(this.id);
         return hash;
     }
-
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -84,5 +98,5 @@ public class Pedido implements Serializable {
         }
         return true;
     }
-
+    
 }
