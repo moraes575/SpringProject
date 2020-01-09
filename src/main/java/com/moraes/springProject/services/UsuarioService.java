5293408fa85f323c6917ee640c4084392ee79504
Matheus Moraes <matheus.moraes111@outlook.com>
2020-01-09 20:56:51 -0300

    Atualização de Usuario

--- src/main/java/com/moraes/springProject/services/UsuarioService.java
+++ src/main/java/com/moraes/springProject/services/UsuarioService.java
@@ -9,25 +9,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
-
+    
     @Autowired
     private UsuarioRepository repository;
-
+    
     public List<Usuario> selectAll() {
         return repository.findAll();
     }
-
+    
     public Usuario selectById(Long id) {
         Optional<Usuario> obj = repository.findById(id);
         return obj.get();
     }
-
+    
     public Usuario insert(Usuario obj) {
         return repository.save(obj);
     }
-
+    
     public void delete(Long id) {
         repository.deleteById(id);
     }
-
+    
+    public Usuario update(Long id, Usuario obj) {
+        Usuario entidade = repository.getOne(id);
+        updateData(entidade, obj);
+        return repository.save(entidade);
+    }
+    
+    private void updateData(Usuario entidade, Usuario obj) {
+        entidade.setNome(obj.getNome());
+        entidade.setEmail(obj.getEmail());
+        entidade.setTelefone(obj.getTelefone());
+    }
+    
 }
